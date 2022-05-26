36c7edb0dd786f4bf8fe871525a145b692b3a3d7
ArtemStepanov1 <sparta.start@gmail.com>
2022-05-27 01:41:22 +0300

    complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+const images = galleryItems.map(({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a  class="gallery__link" href="${original}">
+        <img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}'</img>
+        </a></div>`
+).join('');
+    
+gallery.insertAdjacentHTML('beforeend',images);
+
+// open
+gallery.addEventListener('click', openGallery);
+
+function openGallery(e) {
+    e.preventDefault()
+    
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
+    instance.show()
+};
+
+//close
